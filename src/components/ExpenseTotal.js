1d6896921ef74dd9e142892f98d04000279cfadf
MonicaZhang1 <71800052+MonicaZhang1@users.noreply.github.com>
2022-09-08 21:55:07 -0400

    init commit

---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const ExpenseTotal = () => {
+    const {expenses} = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total,item)=>{
+        return (total+=item.cost);
+
+    }, 0); //set starting value to 0
+
+    return (
+        <div className='alert alert-primary'>
+            <span> Spent So Far: ${totalExpenses}</span>
+        </div>
+    );
+};
+
+export default ExpenseTotal;
